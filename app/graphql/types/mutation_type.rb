f787f0819c98854bca2698451f27488e4f497dec
jose arjona <jarjona@kaufmanrossin.com>
2021-10-21 16:20:00 -0400

    TRAKR-7 didnt see stash change

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
-<<<<<<< Updated upstream
-    field :create_session, mutation: Mutations::CreateSession
-=======
     include Resolvers::Mutations::Sessions
     # include Resolvers::Mutations::UpdateSession
->>>>>>> Stashed changes
   end
 end
 
